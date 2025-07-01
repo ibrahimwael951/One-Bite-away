ccd7b8fb74590f15bda7c0732bd159bca79a5269
ibrahimwael951 <ibrahimwael809@gmail.com>
2025-07-01 03:35:32 +0300

    edit contact form and conected it with real server email sender

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,49 @@
 "use client";
-import React from "react";
+import React, { FormEvent, useState } from "react";
 import Link from "next/link";
 import { Hamburger } from "lucide-react";
 import LinksData from "@/Data/Links.json";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import { useUser } from "@clerk/nextjs";
 const Footer = () => {
   const { isSignedIn } = useUser();
+  const [success, setSuccess] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [massage, setMassage] = useState("");
+  const [error,setError]=useState(false)
+  
+  async function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        access_key: `${process.env.NEXT_PUBLIC_CONTACT_KEY}`,
+        from: "One-Bite-Away",
+        name,
+        email,
+        massage,
+      }),
+    });
+
+    const result = await response.json();
+    if (result.success) {
+      console.log("Message sent successfully!");
+      setName("");
+      setEmail("");
+      setMassage("");
+      setSuccess(true);             
+    }
+    else{
+      setError(true)
+    }
+  }
+
   return (
     <footer className="w-full min-h-96 flex gap-y-14 flex-col md:flex-row  justify-between items-center p-10 bg-neutral-200 dark:bg-neutral-900 overflow-hidden">
       <div className="flex flex-col items-center md:items-start  gap-6 justify-center">
@@ -57,31 +94,70 @@ const Footer = () => {
           okay :)
         </h1>
 
-        <form className="flex flex-col justify-center gap-2">
+        <form
+          onSubmit={handleSubmit}
+          className="relative flex flex-col justify-center gap-2"
+        >
+          <AnimatePresence>
+            {success && (
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                className="absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center bg-yellow-500 rounded-2xl  "
+              >
+                <h1 className="text-2xl font-semibold">Massage Send Successfully :)</h1>
+                <p className="text-lg">Ty for Ur Massage </p>
+              </motion.div>
+            )}
+            {error && (
+                   <motion.div
+                   initial={{ opacity: 0 }}
+                   animate={{ opacity: 1 }}
+                   exit={{ opacity: 0 }}
+                   className="absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center bg-yellow-500 rounded-2xl  "
+                 >
+                   <h1 className="text-2xl font-semibold">Some thing went wrong :(</h1>
+                   <p className="text-lg">Try again later</p>
+                 </motion.div>
+            )}
+          </AnimatePresence>
           <div className="flex gap-2 w-full">
             <motion.input
-              whileFocus={{ scale: 1.04 }}
+              required
+              name="name"
+              whileFocus={{ scale: 1.01 }}
+              value={name}
+              onChange={(e) => setName(e.target.value)}
               type="text"
               placeholder="Ur Name"
-              className="w-2/4 bg-yellow-500 rounded-2xl p-2 placeholder:text-black placeholder:opacity-70 text-black outline-none"
+              className="w-2/4 border-2 border-yellow-500    rounded-2xl p-2 outline-none"
             />
             <motion.input
-              whileFocus={{ scale: 1.04 }}
+              required
+              name="email"
+              whileFocus={{ scale: 1.01 }}
               type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               placeholder="Ur Email"
-              className="w-2/4 bg-yellow-500 rounded-2xl p-2 placeholder:text-black placeholder:opacity-70 text-black outline-none"
+              className="w-2/4 border-2 border-yellow-500    rounded-2xl p-2 outline-none"
             />
           </div>
           <motion.textarea
-            whileFocus={{ scale: 1.04 }}
+            required
+            name="massage"
+            value={massage}
+            onChange={(e) => setMassage(e.target.value)}
+            whileFocus={{ scale: 1.01 }}
             placeholder="Ur Massage"
-            className="bg-yellow-500 rounded-2xl p-2 placeholder:text-black placeholder:opacity-70 text-black outline-none"
+            className="border-2 border-yellow-500 rounded-2xl p-2 outline-none"
           />
           <motion.button
             whileHover={{ scale: 1.06 }}
             whileTap={{ scale: 0.96 }}
             type="submit"
-            className="p-3 w-full border border-yellow-500 rounded-2xl hover:bg-yellow-500"
+            className="p-4 w-full  rounded-2xl bg-yellow-500 uppercase font-semibold text-xl"
           >
             Submit
           </motion.button>
